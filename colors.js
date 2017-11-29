e894ffa268a6b07f4c8d0fcd563a08b77a598167
egoing <egoing@gmail.com>
2017-11-29 23:22:07 +0900

    파일로 정리 정돈하기

--- colors.js
+++ colors.js
@@ -1,19 +1,22 @@
 var Links = {
   setColor:function(color){
-    var alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length){
-      alist[i].style.color = color;
-      i = i + 1;
-    }
+    // var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i < alist.length){
+    //   alist[i].style.color = color;
+    //   i = i + 1;
+    // }
+    $('a').css('color', color);
   }
 }
 var Body = {
   setColor:function (color){
-    document.querySelector('body').style.color = color;
+    //document.querySelector('body').style.color = color;
+    $('body').css('color', color);
   },
   setBackgroundColor:function (color){
-    document.querySelector('body').style.backgroundColor = color;
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
   }
 }
 function nightDayHandler(self){
